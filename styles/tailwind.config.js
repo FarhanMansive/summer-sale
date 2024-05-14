d5dc8eaa91a1628253d3543f4ea7313004483985
FarhanMansive <farhanhasan7610@gmail.com>
2024-05-14 12:36:33 +0600

    first commit

---
+++ styles/tailwind.config.js
@@ -0,0 +1,23 @@
+<script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+  </script>
+
+
+
+
+module.exports = {
+    //...
+    daisyui: {
+      themes: ["light"],
+    },
+  }
+
+
